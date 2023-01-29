9976ab8cb3c2d7c97ece4cbd76714089c7dd2ee9
Volodymyr Panov <vpanov25@gmail.com>
2023-01-29 19:58:13 +0100

    added queryset to views.py

--- books/views.py
+++ books/views.py
@@ -1,9 +1,11 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets
 
+from books.models import Books
 from books.permissions import IsAdminOrIfAuthenticatedReadOnly
 from books.serializers import BooksSerializer
 
 
 class BooksViewSet(viewsets.ModelViewSet):
+    queryset = Books.objects.all()
     serializer_class = BooksSerializer
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
