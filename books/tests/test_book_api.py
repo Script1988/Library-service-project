06049cba18c2980da866dcb7b9759f62fa944978
Volodymyr Panov <vpanov25@gmail.com>
2023-02-08 21:07:47 +0100

    Added tests to books app

--- books/tests/test_book_api.py
+++ books/tests/test_book_api.py
@@ -5,7 +5,7 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 from books.models import Books
-from books.serializers import BooksSerializer
+from books.serializers import BooksListSerializer
 
 BOOK_URL = reverse("books:books-list")
 
@@ -52,3 +52,33 @@ class AuthenticatedBookApiTests(TestCase):
         result = self.client.post(BOOK_URL, default)
         self.assertEqual(result.status_code, status.HTTP_403_FORBIDDEN)
 
+    def test_list_books(self):
+        sample_book()
+        sample_book()
+        result = self.client.get(BOOK_URL)
+        books = Books.objects.all()
+        serializer = BooksListSerializer(books, many=True)
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        self.assertEqual(result.data, serializer.data)
+
+
+class AdminBooksApiTests(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            "admin@admin.com",
+            "testpass",
+            is_staff=True,
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_create_book_allowed(self):
+        default = {
+            "title": "Test book",
+            "author": "Test author",
+            "cover": "Hard",
+            "inventory": 3,
+            "daily_fee": 2.50,
+        }
+        result = self.client.post(BOOK_URL, default)
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
